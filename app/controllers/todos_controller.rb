fe6d95ecde1131585866029ee1686c1272e0053a
ShakthiGanesh <shakthi012000@gmail.com>
2020-03-12 16:18:43 +0530

    Rendering list of todos in formatted manner

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
     def index
-        render plain: Todo.all.to_a
+        render plain: Todo.order(:due_date).map {|todo| todo.to_look_pleasant }.join("\n")
     end
 end 
