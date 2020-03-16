aa67484689b9089e0d4fcfde8b34efc24dbc00a1
ShakthiGanesh <shakthi012000@gmail.com>
2020-03-16 20:14:29 +0530

    Created a new user table in /users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    def index
+        render plain: User.all.map{|user| user.to_look_pleasant}.join("\n")
+    end
+
+    def create
+        name=params[:name]
+        email=params[:email]
+        password=params[:password]
+        new_user=User.create!(name: name,email: email, password: password)
+        render plain: "A new user is created with the id #{new_user.id}"
+    end    
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_161354) do
+ActiveRecord::Schema.define(version: 2020_03_16_142008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 2020_03_11_161354) do
   end
 
   create_table "user", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password"
