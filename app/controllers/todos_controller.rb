c1f511ffc87d00f41fb6f02afdbeb1286a0d1676
ShakthiGanesh <shakthi012000@gmail.com>
2020-03-18 21:45:42 +0530

    Final code

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,34 +1,33 @@
 class TodosController < ApplicationController
-    skip_before_action :verify_authenticity_token
-    
-    def index
-        render plain: Todo.order(:due_date).map {|todo| todo.to_look_pleasant }.join("\n")
-    end
-    
-    def create
-        todo_text=params[:todo_text]
-        due_date=DateTime.parse(params[:due_date])
-        new_todo=Todo.create!(
-            todo_text: todo_text,
-            due_date: due_date,
-            completed: false 
-            )
-        render plain:"The new todo is created with id #{new_todo.id}"
-    end
+  skip_before_action :verify_authenticity_token
 
-    def show
-        id = params[:id]
-        todo=Todo.find(id)
-        render plain: todo.to_look_pleasant
-    end
+  def index
+    render plain: Todo.order(:due_date).map { |todo| todo.to_look_pleasant }.join("\n")
+  end
 
-    def update
-        completed=params[:completed]
-        id=params[:id]
-        todo=Todo.find(id)
-        todo.completed=completed
-        todo.save!
-        render plain:"Updated Todo completed status to #{completed}"
-    end
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(
+      todo_text: todo_text,
+      due_date: due_date,
+      completed: false,
+    )
+    render plain: "The new todo is created with id #{new_todo.id}"
+  end
 
-end 
+  def show
+    id = params[:id]
+    todo = Todo.find(id)
+    render plain: todo.to_look_pleasant
+  end
+
+  def update
+    completed = params[:completed]
+    id = params[:id]
+    todo = Todo.find(id)
+    todo.completed = completed
+    todo.save!
+    render plain: "Updated Todo completed status to #{completed}"
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,25 @@
 class UsersController < ApplicationController
-    skip_before_action :verify_authenticity_token
-    
-    def index
-        render plain: User.all.map{|user| user.to_look_pleasant}.join("\n")
-    end
+  skip_before_action :verify_authenticity_token
 
-    def create
-        name=params[:name]
-        email=params[:email]
-        password=params[:password]
-        new_user=User.create!(name: name,email: email, password: password)
-        render plain: "A new user is created with the id #{new_user.id}"
-    end 
+  def index
+    render plain: User.all.map { |user| user.to_look_pleasant }.join("\n")
+  end
 
-    def  show
-        id=params[:id]
-        user=User.find(id) 
-        render plain:"User with id #{id} : #{user.to_look_pleasant}"
-    end 
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(name: name, email: email, password: password)
+    render plain: "A new user is created with the id #{new_user.id}"
+  end
 
-    def login
-        render plain: User.where(email: params[:email] ,password: params[:password]).exists?
-    end
+  def show
+    id = params[:id]
+    user = User.find(id)
+    render plain: "User with id #{id} : #{user.to_look_pleasant}"
+  end
+
+  def login
+    render plain: User.where(email: params[:email], password: params[:password]).exists?
+  end
 end
