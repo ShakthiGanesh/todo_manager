5063a83b7a91af4848576b7294315ad403947f12
ShakthiGanesh <shakthi012000@gmail.com>
2020-04-15 15:49:06 +0530

    Final todo Application

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,14 +10,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    User.create!(
+    new_user = User.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       password: params[:password],
-
     )
-    redirect_to "/"
+    if new_user.save
+      redirect_to todos_path
+      session[:current_user_id] = new_user.id
+      @current_user = new_user
+    else
+      flash[:error] = new_user.errors.full_messages.join(" & ")
+      redirect_to new_user_path
+    end
   end
 
   def show
